b4840155bb7e37f3d9e9302587aa68efd8eadf06
CBroaders <67294112+CBroaders12@users.noreply.github.com>
2020-10-06 14:00:28 -0400

    clarified content & updated template

--- public/script.js
+++ public/script.js
@@ -13,7 +13,6 @@ const dayNames = ["Sun", "Mon", "Tues", "Wed", "Thurs", "Fri", "Sat"];
 let secondsPerDay = 24 * 60 * 60; //for calculating Unix time
 let today = new Date(); // Current time
 let todaySeconds = Math.floor(today / 1000); // Unix time in seconds
-// let fiveDaysAgo = new Date(today - 4 * (secondsPerDay * 1000));
 
 // Declare variables for HTML Tags
 let searchForm = document.querySelector("form");
@@ -34,10 +33,6 @@ longSlider.oninput = () => (longOutput.innerHTML = longSlider.value);
 
 // Fetch Data
 searchForm.addEventListener("submit", fetchWeather);
-
-// TODO: Set location;
-// latitude = latSlider.value;
-// longitude = longSlider.value;
 
 function fetchWeather(e) {
 	e.preventDefault();
@@ -80,24 +75,40 @@ function displayEarthInfo(json) {
 	let dateString = `${dayNames[day]}, ${monthNames[month]} ${date}`;
 	let location = `${latSlider.value}\u00b0, ${longSlider.value}\u00b0`;
 
-	//Calculate Avg, min, max temp
+	//Get measurements and calculate averages
 	let temps = [];
+	let avgPressure = 0;
+	let avgWindSpeed = 0;
 	let avgTemp = 0;
 	let minTemp;
 	let maxTemp;
+
 	for (hour of hourlyData) {
 		temps.push(hour.temp);
 		avgTemp += hour.temp;
+
+		avgPressure += hour.pressure;
+
+		avgWindSpeed += hour.wind_speed;
 	}
+
+	// Calculate max, min, and average temperature
 	minTemp = Math.round(Math.min(...temps));
 	maxTemp = Math.round(Math.max(...temps));
 
 	avgTemp /= hourlyData.length;
 	avgTemp = Math.round(avgTemp);
 
-	// TODO: Pull wind info
+	// Calculate average wind speed (measured in mph)
+	avgWindSpeed /= hourlyData.length;
+	avgWindSpeed = Math.round(avgWindSpeed * 100) / 100; //Round to 2 decimal places
+	console.log(`Earth Wind Speed: ${avgWindSpeed} mph`);
 
-	// TODO: Pull pressure info
+	// Calculate average pressure (measured in hPa (1 hPa = 100 Pa))
+	avgPressure /= hourlyData.length;
+	avgPressure *= 100; //Convert to Pascals
+	avgPressure = Math.round(avgPressure * 100) / 100; // Round to 2 decimal places
+	console.log(`Earth Pressure: ${avgPressure} Pa`);
 
 	let earthCard = buildCard(location, timeZone, dateString, avgTemp, minTemp, maxTemp);
 
@@ -120,33 +131,28 @@ function displayMarsInfo(json) {
 	// let timeStamp = new Date(solData.Last_UTC); // Not used
 	let dateString = `Sol ${lastSol}`; // Show the Sol of the Insight mission instead of the Earth Date
 
-	// Get temperature readings in Fahrenheit
+	// Get temperature readings in Celsius and convert to Fahrenheit
 	let avgTemp = Math.round(1.8 * solData.AT.av + 32);
 	let minTemp = Math.round(1.8 * solData.AT.mn + 32);
 	let maxTemp = Math.round(1.8 * solData.AT.mx + 32);
 
-	// TODO: Pull wind info
+	// Pull wind info (speed in m/s)
 	if (solData.HWS) {
-		let windSpeed = solData.HWS.av;
-		console.log(windSpeed);
-	}
-	if (solData.WD) {
-		let windDirection = solData.WD.most_common.compass_point;
-		console.log(windDirection);
+		let windSpeed = solData.HWS.av * 2.237; // Convert to mph for comparison
+		windSpeed = Math.round(windSpeed * 100) / 100; // Round to 1 Decimal Place
+		console.log(`Mars Wind Speed: ${windSpeed} mph`);
 	}
 
-	// TODO: Pull pressure info
+	// Pull pressure info (measured in Pa)
 	if (solData.PRE) {
-		let avgPressure = Math.round(solData.PRE.av);
-		console.log(avgPressure);
+		let avgPressure = Math.round(solData.PRE.av * 100) / 100;
+		console.log(`Mars Pressure: ${avgPressure} Pa`);
 	}
 
 	let marsCard = buildCard("4.5\u00b0 135.6\u00b0", "Elysium Planitia", dateString, avgTemp, minTemp, maxTemp);
 
 	marsDiv.appendChild(marsCard);
 }
-// }
-// }
 
 function buildCard(location, locationExtra, date, avgTemp, minTemp, maxTemp) {
 	let card = document.createElement("div");
@@ -165,7 +171,7 @@ function buildCard(location, locationExtra, date, avgTemp, minTemp, maxTemp) {
 	cardBody.classList.add("card-body");
 	locationText.classList.add("card-title", "text-center", "my-2");
 	locationDetail.classList.add("card-subtitle", "text-center");
-	measurementDate.classList.add("card-subtitle");
+	measurementDate.classList.add("card-subtitle", "text-center");
 	avgTempText.classList.add("card-text");
 	minTempText.classList.add("card-text");
 	maxTempText.classList.add("card-text");
